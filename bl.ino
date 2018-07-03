579157bc73ec5a695102fb0f0ed331ebafc19002
Tanya Sethi <26785390+TanyaSethi@users.noreply.github.com>
2018-07-04 00:18:00 +0530

    Add files via upload

---
+++ bl.ino
@@ -0,0 +1,123 @@
+#include<SoftwareSerial.h>
+#include <Servo.h>
+SoftwareSerial mySerial(10,11);
+Servo myservo;
+int pos = 0;
+int calibrationTime = 10;
+long unsigned int lowIn;
+long unsigned int pause = 50;
+boolean lockLow = true;
+boolean takeLowTime;
+char val;
+int pirPin = 12; 
+
+void setup()
+{
+  myservo.attach(9);
+  pinMode(2,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(5,OUTPUT);
+  Serial.begin(9600);
+  mySerial.begin(38400);
+  pinMode(pirPin, INPUT);
+ pinMode(9,OUTPUT);
+  digitalWrite(pirPin, LOW);
+  for(int i = 0; i < calibrationTime; i++) {
+  Serial.print(".");
+  }
+}
+void loop(){
+  //PIR and Gate_servo motor
+  if(digitalRead(pirPin) == HIGH){
+  //Serial.print("Motion Detected");
+  // digitalWrite(9,HIGH);
+for (pos = 0; pos <= 120; pos += 2) { 
+      myservo.write(pos);   
+      delay(50);          
+  }
+if(lockLow){
+
+//makes sure we wait for a transition to LOW before any further output is made:
+
+lockLow = false;
+
+//delay(50);
+
+}
+
+takeLowTime = true;
+
+}
+if(digitalRead(pirPin) == LOW){
+ // digitalWrite(9, LOW);
+  for (pos = 120; pos <= 0;pos -= 2) {
+      myservo.write(pos);      
+      delay(50);                       
+  }
+ //the led visualizes the sensors output pin state
+
+if(takeLowTime){
+
+lowIn = millis(); //save the time of the transition from high to LOW
+
+takeLowTime = false; //make sure this is only done at the start of a LOW phase
+
+}
+
+//if the sensor is low for more than the given pause,
+
+//we assume that no more motion is going to happen
+
+if(!lockLow && millis() - lowIn > pause){
+
+//makes sure this block of code is only executed again after
+
+//a new motion sequence has been detected
+
+lockLow = true;
+
+Serial.print("motion ended"); //output
+
+//delay(50);
+
+}
+
+}
+//bluetooth
+  if (mySerial.available()) {
+    Serial.write(val=mySerial.read());
+    Serial.read();
+    if(val =='1')
+    {
+      if(digitalRead(2)==LOW)
+       digitalWrite(2,HIGH);
+      else
+       digitalWrite(2,LOW);
+    }
+    if(val =='2')
+    {
+      if(digitalRead(5)==LOW)
+      digitalWrite(5,HIGH);
+      else
+      digitalWrite(5,LOW);
+    }
+    if(val == '3')
+    {
+      if(digitalRead(6)==LOW)
+      digitalWrite(6,HIGH);
+      else
+      digitalWrite(6,LOW);
+    }
+       
+    if(val == '4')
+    {
+      if(digitalRead(7)==LOW)
+      digitalWrite(7,HIGH);
+      else
+      digitalWrite(7,LOW);
+    }
+  }
+}
+
+
---
+++ bl2.ino
@@ -0,0 +1,71 @@
+#include<LiquidCrystal.h>
+int Contrast=20;
+LiquidCrystal lcd(10,3,8,9,12,11);
+int state;
+int buttonState = 0;  
+const int buttonPin = 2;
+int flag=0;       
+void stp();
+void fwd();
+void setup()
+{
+    analogWrite(6,Contrast);
+    lcd.begin(16,2);
+    pinMode(7,OUTPUT);                 
+    pinMode(8,OUTPUT);                  
+    pinMode(5,OUTPUT);                  
+    pinMode(6,OUTPUT); 
+    pinMode(13, OUTPUT);        
+    pinMode(buttonPin, INPUT);  
+    Serial.begin(9600);                                             
+}
+void loop() {
+    lcd.setCursor(0,0);
+     lcd.print("Welcome");
+    buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == LOW) { 
+    lcd.clear(); 
+    lcd.setCursor(0,8);
+    lcd.print("Order Ready");
+    fwd();
+    Serial.print("button pressed");
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0,8);
+    lcd.print("Order Delivered");
+    delay(1000);
+    
+  } else {
+   lcd.setCursor(0,0);
+   // lcd.print("Welcome");
+    Serial.print(".....removed");
+    stp();
+  }
+ // delay(3000);
+  //lcd.setCursor(0,0);
+  //lcd.print("Total Bill:100");  
+  
+  
+}                                           //loop() ends here
+void fwd()          // Forward
+{
+  digitalWrite(7,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(8,LOW);
+  
+  digitalWrite(13, HIGH);
+}
+
+void stp()            // Robot STops
+{
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(13, LOW);
+}
+
+
